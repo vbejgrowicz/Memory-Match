fe31b0e17283c86af07c992280aa15b50610a198
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-03-12 18:23:17 -0400

    Add methods to Board class to populate the board with shuffled pairs

--- board.rb
+++ board.rb
@@ -1,3 +1,5 @@
+require './card'
+
 class Board
   def self.generate_size(level)
     board_size = { easy: 4, medium: 6, hard: 8 }
@@ -6,5 +8,31 @@ class Board
   def initialize(size)
     @size = size
     @cards_array = []
+    populate_cards
+  end
+
+  def populate_cards
+    positions = position_ids
+    card_values.each_with_index do |value, index|
+      cards_array << Card.new(value, positions[index])
+    end
+    cards_array
+  end
+  private
+
+  def position_ids
+    ids = []
+    rows = ('A'..'Z').to_a.slice(0, size)
+    rows.each do |letter|
+      (1..size).each { |number| ids << "#{letter}#{number}" }
+    end
+    ids
+  end
+
+  def card_values
+    values = (1..(size * size / 2)).to_a
+    card_values = values.concat(values)
+    card_values.shuffle!
+  end
   end
 end
