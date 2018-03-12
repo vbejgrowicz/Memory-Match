f55219c6511ecfe1a8fa759de145fe8fb27eaaf7
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-03-12 18:15:11 -0400

    Add method to Game class to set game difficulty

---
+++ game.rb
@@ -0,0 +1,19 @@
+class Game
+  attr_reader :player, :board
+
+  def initialize
+    @difficulty = set_difficulty
+
+  end
+  private
+
+  def set_difficulty
+    levels = %i[easy medium hard]
+    puts 'Enter a Difficulty: Easy, Medium, Hard'
+    user_input = gets.chomp.downcase.to_sym
+    return set_difficulty unless levels.include?(user_input)
+    user_input
+  end
+end
+
+Game.new
